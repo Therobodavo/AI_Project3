992afa201fc9e508bfe2fa9bd39abb24ee1ad29b
zhd407108459 <407108459@qq.com>
2019-11-27 01:06:13 +0800

    Auto Compensate Rotation

---
+++ AI_Project/Assets/Scripts/AutoCompensateRotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCompensateRotation : MonoBehaviour
+{
+    public GameObject emptyPrefab;
+
+    void Start()
+    {
+        CompensateRotation();
+    }
+
+    void Update()
+    {
+        CompensateRotation();
+    }
+
+    public void CompensateRotation()
+    {
+        Transform parent = GetComponentInParent<BoxCollider>().transform;
+        if(parent == null)
+        {
+            return;
+        }
+        GameObject go = Instantiate(emptyPrefab, new Vector3(parent.position.x, parent.position.y + 16, parent.position.z), Quaternion.Euler(0, 0, 0));
+        go.transform.SetParent(parent);
+        transform.position = go.transform.position;
+        transform.rotation = go.transform.rotation;
+        Destroy(go);
+
+    }
+}
